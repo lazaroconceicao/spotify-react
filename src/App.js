b5937523fcdeaa358da47c89c4cd795a9f72255d
lazaroconceicao <joselazaroc@gmail.com>
2024-01-31 20:44:56 -0300

    sidebar and footer creation

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import "./App.css";
+import Footer from "./componentes/Footer/Footer";
 import Header from "./componentes/Header/Header";
 import Main from "./componentes/Main/Main";
+import Sidebar from "./componentes/Sidebar/Sidebar";
 
 function App() {
   return (
     <>
+    <Sidebar />
       <Header />
       <Main />
+      <Footer />
     </>
   );
 }
